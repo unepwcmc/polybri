49cc436a497ecb5320fb4a8e45d5b3c1ae6432f6
simaob <simaobelchior@gmail.com>
2011-04-08 14:17:22 +0100

    ups

---
+++ server.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var server = require('http').createServer(function(req, response){
+  fs.readFile(__dirname+'/edit.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'}); 
+    response.write(data);  
+    response.end();
+  });
+});
+server.listen(8080);
+var everyone = require("now").initialize(server);
+
+everyone.now.distributeMessage= function(msg) {
+  everyone.new.receiveMessage(this.now.name, msg);
+}
