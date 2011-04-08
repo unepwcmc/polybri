f31b5d843840a8e7625f36fe118cf558fd24390d
James Cox <th3james@gmail.com>
2011-04-08 14:53:45 +0100

    Added express and njs, provides routing, layouts etc

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-var fs = require('fs'),
-    http = require('http');
-var server = http.createServer(function(req, response){
-  fs.readFile(__dirname+'/public/edit.html', function(err, data){
-    response.writeHead(200, {'Content-Type':'text/html'}); 
-    response.write(data);  
-    response.end();
-  });
+var express = require('express');
+
+var app = express.createServer();
+
+// Configuration
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('edit', {locals: {
+    title: 'NowJS + Express Example'
+  }});
 });
-server.listen(9001);
+
+app.listen(9001);
+
 
 //Node here
-var everyone = require("now").initialize(server);
+var everyone = require("now").initialize(app);
 
 everyone.now.distributeMessage= function(msg) {
   everyone.now.receiveMessage(this.now.name, msg);
