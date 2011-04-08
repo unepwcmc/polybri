fe05cf731ba47fdbe6d485e2963f4e557aa0caf6
James Cox <th3james@gmail.com>
2011-04-08 17:30:48 +0100

    sudocode for now

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  var polys = {}
+
   //Chat stuff
    now.receiveMessage = function(name, message, color){
      $("#chat").append("<br><span style='color: "+color+"'>" + name + "</span>: " + message);
@@ -11,6 +13,11 @@ $(document).ready(function(){
 
   //Polygon stuff
   now.receivePolygon = function(name, GeoJson){
+
+    /* engage sudocode!
+    polys[name].delete
+    polys[name] = polygon.create(GeoJson)
+    */
 
   };
 
