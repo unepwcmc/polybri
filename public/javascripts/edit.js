544a262bb2d066cbf542f052f7afbab0c4e99e49
simaob <simaobelchior@gmail.com>
2011-04-08 17:29:06 +0100

    Fancying the chat window. Repeat the name only if the user was not the last speaker. Don't send if text box is empty

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
 
   //Chat stuff
-   now.receiveMessage = function(name, message, color){
-     $("#chat").append("<br><span style='color: "+color+"'>" + name + "</span>: " + message);
+  now.receiveMessage = function(name, message, color, sameWriter){
+     if(!sameWriter){
+       $("#chat").append("<span style='color: "+color+"'>" + name + "</span>: " + message+"<br />");
+     }
+     else{
+       $("#chat").append(message+"<br />");
+     }
    }
    $("#send").click(function(){
-     now.distributeMessage($("#text-input").val());
-     $("#text-input").val("");
+     if( $("#text-input").val() != ""){
+       now.distributeMessage($("#text-input").val());
+       $("#text-input").val("");
+     }
    });
-
   //Polygon stuff
   now.receivePolygon = function(name, GeoJson){
-
   };
-
 });
   
